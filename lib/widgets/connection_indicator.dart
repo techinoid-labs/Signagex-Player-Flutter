2ccdea724c302ad606b35a8acdce2ac3a8553788
qasim212 <qasim.afzaal@techinoid.com>
2024-09-02 21:03:57 +0500

    add connection indicator widget

---
+++ lib/widgets/connection_indicator.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MqttStatusIndicator extends StatelessWidget {
+  final bool isConnected;
+
+  MqttStatusIndicator({required this.isConnected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        isConnected ? 'Connected' : 'Disconnected',
+        style: TextStyle(
+          fontSize: 24,
+          color: isConnected ? Colors.green : Colors.red,
+        ),
+      ),
+    );
+  }
+}
