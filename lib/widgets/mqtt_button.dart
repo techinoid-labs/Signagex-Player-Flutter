6f3192c4cc186aa4a51bb6aab53d7058733f27e6
qasim212 <qasim.afzaal@techinoid.com>
2024-09-04 18:38:34 +0500

    call mqtt function

--- lib/widgets/mqtt_button.dart
+++ lib/widgets/mqtt_button.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import '../view_models/mqtt_provider.dart';
@@ -6,20 +8,27 @@ class MqttButtons extends StatelessWidget {
   final MqttProvider mqttProvider;
 
   MqttButtons({required this.mqttProvider});
-
+  Map<String, dynamic> body = {
+    "success": "true",
+    "action": 'jvjhjgvfj',
+    "paired": "false",
+    "player_code": "playerCode",
+    "mac_address": "macAddressArray",
+    "sender": 'norwinsol_web'
+  };
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         ElevatedButton(
           onPressed: () {
-            mqttProvider.subscribe('806116');
+            mqttProvider.subscribe('85a1d2');
           },
           child: Text('Subscribe'),
         ),
         ElevatedButton(
           onPressed: () {
-            mqttProvider.publish('806116', 'Hello MQTT');
+            mqttProvider.publish('85a1d2', jsonEncode(body));
           },
           child: Text('Publish'),
         ),
@@ -28,6 +37,12 @@ class MqttButtons extends StatelessWidget {
             mqttProvider.disconnect();
           },
           child: Text('Disconnect'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            mqttProvider.connect(); // Connect or reconnect
+          },
+          child: Text('Connect'),
         ),
       ],
     );
