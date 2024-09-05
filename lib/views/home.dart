fd592402dd05199c999d9ca8f26e58e16741449f
qasim212 <qasim.afzaal@techinoid.com>
2024-09-05 17:25:33 +0500

    recieve and publish mqtt data and show on screen using provider

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,42 +2,45 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-import 'package:digital_signage/widgets/connection_indicator.dart';
-import 'package:digital_signage/widgets/mqtt_button.dart';
-
-import '../view_models/mqtt_provider.dart';
-
+import '../view_models/mqtt_view_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      final mqttProvider = Provider.of<MqttProvider>(context, listen: false);
-      mqttProvider.init();
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final mqttProvider = Provider.of<MqttProvider>(context);
-
     return Scaffold(
-      appBar: AppBar(title: const Text('MQTT Client')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          MqttStatusIndicator(isConnected: mqttProvider.isConnected),
-          const SizedBox(height: 20),
-          MqttButtons(mqttProvider: mqttProvider),
-        ],
+      appBar: AppBar(
+        title: const Text('MQTT Code'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('Data received:',
+                style: TextStyle(color: Colors.black, fontSize: 25)),
+            Consumer<MqttViewModel>(
+              builder: (context, mqttViewModel, child) {
+                return Text(
+                  mqttViewModel.receivedMessage,
+                  style: const TextStyle(color: Colors.deepPurpleAccent, fontSize: 10),
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Provider.of<MqttViewModel>(context, listen: false).publishMessage("Hello MQTT");
+              },
+              child: Text("Publish Message"),
+            ),
+          ],
+        ),
       ),
     );
   }
