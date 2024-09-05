c94df02b880f020603e9a5e86f8aaaaa92f4be52
qasim212 <qasim.afzaal@techinoid.com>
2024-09-05 17:26:42 +0500

    update mqtt client service

--- lib/services/mqtt_client_service.dart
+++ lib/services/mqtt_client_service.dart
@@ -1,61 +1,109 @@
+import 'package:flutter/material.dart';
+
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 
+const String mqttBroker = 'broker.emqx.io';
+const int mqttPort = 1883;
+
 class MqttClientService {
   late MqttServerClient _client;
+  final ValueNotifier<String> receivedMessageNotifier = ValueNotifier<String>('');
 
-  MqttClientService(String broker, int port) {
-    _client = MqttServerClient(broker, 'uniqueClientID_${DateTime.now().millisecondsSinceEpoch}');
-    _client.port = port;
-    _client.keepAlivePeriod = 20;
+  MqttClientService() {
+    _initializeClient();
+  }
 
-    _client.onConnected = () {
-      print('MQTT client connected');
-    };
+  void _initializeClient() {
+    _client = MqttServerClient.withPort(mqttBroker, 'uniqueClientID_${DateTime.now().millisecondsSinceEpoch}', mqttPort);
 
-    _client.onDisconnected = () {
-      print('MQTT client disconnected');
-    };
+    _client.logging(on: true);
+    _client.onConnected = onConnected;
+    _client.onDisconnected = onDisconnected;
+    _client.onUnsubscribed = onUnsubscribed;
+    _client.onSubscribed = onSubscribed;
+    _client.onSubscribeFail = onSubscribeFail;
+    _client.pongCallback = pong;
+    _client.keepAlivePeriod = 60;
+    _client.setProtocolV311();
+  }
 
-    _client.onSubscribed = (String topic) {
-      print('Successfully subscribed to topic: $topic');
-    };
+  void onConnected() {
+    print('MQTT_LOGS:: Connected');
+  }
+
+  void onDisconnected() {
+    print('MQTT_LOGS:: Disconnected');
+  }
+
+  void onSubscribed(String topic) {
+    print('MQTT_LOGS:: Subscribed topic: $topic');
+  }
+
+  void onSubscribeFail(String topic) {
+    print('MQTT_LOGS:: Failed to subscribe $topic');
+  }
+
+  void onUnsubscribed(String? topic) {
+    print('MQTT_LOGS:: Unsubscribed topic: $topic');
+  }
+
+  void pong() {
+    print('MQTT_LOGS:: Ping response client callback invoked');
   }
 
   Future<void> connect() async {
+    final connMessage = MqttConnectMessage()
+        .withWillTopic('willtopic')
+        .withWillMessage('Will message')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+
+    print('MQTT_LOGS::Mosquitto client connecting....');
+    _client.connectionMessage = connMessage;
+
     try {
       await _client.connect();
-      print('MQTT client connected');
+      if (_client.connectionStatus!.state == MqttConnectionState.connected) {
+        print('MQTT_LOGS::Mosquitto client connected');
+        subscribe('85a1d2');
+      } else {
+        print('MQTT_LOGS::ERROR Mosquitto client connection failed - disconnecting, status is ${_client.connectionStatus}');
+        _client.disconnect();
+      }
     } catch (e) {
-      print('Exception during connect: $e');
+      print('Exception: $e');
       _client.disconnect();
     }
   }
 
   void disconnect() {
-    _client.disconnect();
-    print('MQTT client disconnected');
-  }
-
-  void subscribe(String topic, MqttQos qos) {
-    try {
-      _client.subscribe(topic, qos);
-      print('Subscribed to topic: $topic with QoS: $qos');
-    } catch (e) {
-      print('Exception during subscribe: $e');
+    if (_client.connectionStatus?.state == MqttConnectionState.connected) {
+      _client.disconnect();
+      print('MQTT_LOGS:: Disconnected');
     }
   }
 
-  void publish(String topic, String message, MqttQos qos) {
-    try {
-      final builder = MqttClientPayloadBuilder();
-      builder.addString(message);
-      _client.publishMessage(topic, qos, builder.payload!);
-      print('Published message to topic: $topic with QoS: $qos');
-    } catch (e) {
-      print('Exception during publish: $e');
-    }
+  void subscribe(String topic) {
+    print('MQTT_LOGS:: Subscribing to the topic: $topic');
+    _client.subscribe(topic, MqttQos.atMostOnce);
+
+    _client.updates?.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+      final recMess = c![0].payload as MqttPublishMessage;
+      final pt = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      receivedMessageNotifier.value = pt;
+      print('MQTT_LOGS:: New data arrived: topic is <${c[0].topic}>, payload is $pt');
+    });
   }
 
-  MqttServerClient get client => _client;
+  void publish(String message) {
+    const pubTopic = '85a1d2';
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+
+    if (_client.connectionStatus?.state == MqttConnectionState.connected) {
+      _client.publishMessage(pubTopic, MqttQos.atMostOnce, builder.payload!);
+      print('MQTT_LOGS:: Published message: $message');
+    }
+  }
 }
