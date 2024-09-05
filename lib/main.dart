473f6cd9602cacdaddab5f25a34e9ee0c75625b1
qasim212 <qasim.afzaal@techinoid.com>
2024-09-05 17:26:00 +0500

    update main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:provider/provider.dart';
-
+import 'package:digital_signage/provider/main_provider.dart';
 import 'package:digital_signage/views/home.dart';
 
-import '../view_models/mqtt_provider.dart';
-
-import 'services/mqtt_client_service.dart';
-import 'utils/constants.dart';
-
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final mqttClientService = MqttClientService(mqttBroker, mqttPort);
-  final mqttProvider = MqttProvider(mqttClientService);
-
-  await mqttProvider.init();
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => mqttProvider,
-      child: MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MQTT Client',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+void main() {
+  runApp(MqttProvider(
+    child: MaterialApp(
       home: HomeScreen(),
-    );
-  }
+    ),
+  ));
 }
