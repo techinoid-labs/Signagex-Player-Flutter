0a885e7372f649e490c2f9d9611434bc8524d2a5
qasim212 <qasim.afzaal@techinoid.com>
2024-09-06 21:56:12 +0500

    add ssl certifcate

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:digital_signage/provider/main_provider.dart';
-import 'package:digital_signage/views/home.dart';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 void main() {
-  runApp(MqttProvider(
-    child: MaterialApp(
-      home: HomeScreen(),
+   HttpOverrides.global = MyHttpOverrides();
+  runApp(
+    MaterialApp(
+      home: MqttProvider(
+        child: MyHomePage(), 
+      ),
     ),
-  ));
+  );
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('MQTT App')),
+      body: const Center(child: Text('Welcome to the MQTT App!')),
+    );
+  }
 }
