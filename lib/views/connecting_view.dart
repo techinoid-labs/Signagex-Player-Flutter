998dd33915369c402d0f6ba4eb2a02a720737fb0
qasim212 <qasim.afzaal@techinoid.com>
2024-09-10 19:56:38 +0500

    update connecting view screen

---
+++ lib/views/connecting_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:digital_signage/widgets/center_image_widget.dart';
+import 'package:digital_signage/widgets/text_widget.dart';
+
+class ConnectingView extends StatefulWidget {
+  const ConnectingView({super.key});
+
+  @override
+  State<ConnectingView> createState() => _ConnectingViewState();
+}
+
+class _ConnectingViewState extends State<ConnectingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+         
+          Image.asset(
+            "assets/images/background.png",
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          ),
+        
+          const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomImageWidget(
+                  imagePath: 'assets/images/Wifi.png',
+                ),
+                SimpleText(
+                  text: "Connecting...",
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                SimpleText(
+                  text:
+                      "Wifi is still trying to connect, but it’s taking longer than normal.\nCheck that your Wifi is on and connected.",
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
