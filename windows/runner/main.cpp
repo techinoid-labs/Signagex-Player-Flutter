8d4abde3c4be1826917e9402057a72bbf2c43262
qasim212 <qasim.afzaal@techinoid.com>
2024-09-23 15:06:52 +0500

    Fix windowa issues

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,45 +1,43 @@
-#include <iphlpapi.h>
+#include <flutter/dart_project.h>
+#include <flutter/flutter_view_controller.h>
 #include <windows.h>
-#include <string>
-#include "flutter/method_channel.h"
-#include "flutter/standard_method_codec.h"
 
-// Helper function to convert MAC address to a string
-std::string GetMacAddress() {
-    PIP_ADAPTER_INFO AdapterInfo;
-    AdapterInfo = (IP_ADAPTER_INFO*)malloc(sizeof(IP_ADAPTER_INFO));
-    DWORD dwBufLen = sizeof(IP_ADAPTER_INFO);
+#include "flutter_window.h"
+#include "utils.h"
 
-    // Make an initial call to GetAdaptersInfo to get the necessary size into dwBufLen
-    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == ERROR_BUFFER_OVERFLOW) {
-        AdapterInfo = (IP_ADAPTER_INFO*)malloc(dwBufLen);
-    }
+int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
+                      _In_ wchar_t *command_line, _In_ int show_command) {
+  // Attach to console when present (e.g., 'flutter run') or create a
+  // new console when running with a debugger.
+  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+    CreateAndAttachConsole();
+  }
 
-    if (GetAdaptersInfo(AdapterInfo, &dwBufLen) == NO_ERROR) {
-        // The MAC address is stored in the Address member of the IP_ADAPTER_INFO structure.
-        char mac_addr[18];
-        snprintf(mac_addr, sizeof(mac_addr), "%02X:%02X:%02X:%02X:%02X:%02X",
-                 AdapterInfo->Address[0], AdapterInfo->Address[1],
-                 AdapterInfo->Address[2], AdapterInfo->Address[3],
-                 AdapterInfo->Address[4], AdapterInfo->Address[5]);
-        free(AdapterInfo);
-        return std::string(mac_addr);
-    }
+  // Initialize COM, so that it is available for use in the library and/or
+  // plugins.
+  ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
-    free(AdapterInfo);
-    return "Error";
-}
+  flutter::DartProject project(L"data");
 
-// Method Channel implementation
-void RegisterMacAddressMethod(flutter::MethodChannel<flutter::EncodableValue>* channel) {
-    channel->SetMethodCallHandler(
-            [](const flutter::MethodCall<flutter::EncodableValue>& call,
-               std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-                if (call.method_name().compare("getMacAddress") == 0) {
-                    std::string mac_address = GetMacAddress();
-                    result->Success(flutter::EncodableValue(mac_address));
-                } else {
-                    result->NotImplemented();
-                }
-            });
+  std::vector<std::string> command_line_arguments =
+      GetCommandLineArguments();
+
+  project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
+
+  FlutterWindow window(project);
+  Win32Window::Point origin(10, 10);
+  Win32Window::Size size(1280, 720);
+  if (!window.Create(L"flutter_application_2", origin, size)) {
+    return EXIT_FAILURE;
+  }
+  window.SetQuitOnClose(true);
+
+  ::MSG msg;
+  while (::GetMessage(&msg, nullptr, 0, 0)) {
+    ::TranslateMessage(&msg);
+    ::DispatchMessage(&msg);
+  }
+
+  ::CoUninitialize();
+  return EXIT_SUCCESS;
 }
