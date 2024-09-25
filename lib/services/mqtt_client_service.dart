ea1379e6d2116790d6f7a61a5e525c552eaebb85
qasim212 <qasim.afzaal@techinoid.com>
2024-09-25 20:53:21 +0500

    handle reboot for all platforms

--- lib/services/mqtt_client_service.dart
+++ lib/services/mqtt_client_service.dart
@@ -1,21 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
+
+import 'package:digital_signage/utils/globle_variable.dart';
 
 const String mqttBroker = 'broker.emqx.io';
 const int mqttPort = 1883;
 
 class MqttClientService {
   late MqttServerClient _client;
-  final ValueNotifier<String> receivedMessageNotifier = ValueNotifier<String>('');
+  final ValueNotifier<String> receivedMessageNotifier =
+      ValueNotifier<String>('');
 
   MqttClientService() {
     _initializeClient();
   }
 
   void _initializeClient() {
-    _client = MqttServerClient.withPort(mqttBroker, 'uniqueClientID_${DateTime.now().millisecondsSinceEpoch}', mqttPort);
+    _client = MqttServerClient.withPort(mqttBroker,
+        'uniqueClientID_${DateTime.now().millisecondsSinceEpoch}', mqttPort);
 
     _client.logging(on: true);
     _client.onConnected = onConnected;
@@ -66,9 +74,9 @@ class MqttClientService {
       await _client.connect();
       if (_client.connectionStatus!.state == MqttConnectionState.connected) {
         print('MQTT_LOGS::Mosquitto client connected');
-
       } else {
-        print('MQTT_LOGS::ERROR Mosquitto client connection failed - disconnecting, status is ${_client.connectionStatus}');
+        print(
+            'MQTT_LOGS::ERROR Mosquitto client connection failed - disconnecting, status is ${_client.connectionStatus}');
         _client.disconnect();
       }
     } catch (e) {
@@ -90,13 +98,83 @@ class MqttClientService {
 
     _client.updates?.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
       final recMess = c![0].payload as MqttPublishMessage;
-      final pt = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+      final pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
       receivedMessageNotifier.value = pt;
-      print('MQTT_LOGS:: New data arrived: topic is <${c[0].topic}>, payload is $pt');
+      print(
+          'MQTT_LOGS:: New data arrived: topic ...$globleTopic.... <${c[0].topic}>, payload is $pt');
+      final jsonObj = jsonDecode(pt);
+
+      if (jsonObj["action"] == "action_reboot") {
+        print("action rebooot");
+        if (Platform.isMacOS) {
+          _rebootDeviceForMacOS();
+        } else if (Platform.isAndroid) {
+          print("i am here for andorind");
+          rebootDeviceForAndroid();
+        } else if (Platform.isWindows) {
+          rebootDeviceForWindows();
+        } else if (Platform.isLinux) {
+          rebootDeviceForLinux();
+        }
+      }
     });
   }
 
-  void publish(String topic,String message) {
+  Future<String> rebootDeviceForLinux() async {
+    print("Attempting to restart the device...");
+
+    // Execute the reboot command
+    final result = await Process.run('pkexec', ['systemctl', 'reboot']);
+
+    print('Exit code: ${result.exitCode}');
+    print('Stdout: ${result.stdout}');
+    print('Stderr: ${result.stderr}');
+
+    if (result.exitCode != 0) {
+      return 'Error: ${result.stderr}';
+    }
+
+    return 'Reboot command executed successfully';
+  }
+
+  Future<void> rebootDeviceForWindows() async {
+    try {
+      publish(globleTopic, "success");
+      final result = await Process.run(
+          'powershell', ['-Command', 'Restart-Computer -Force']);
+
+      if (result.exitCode != 0) {
+        print('Error: ${result.stderr}');
+      } else {
+        print('Device is rebooting...');
+      }
+    } catch (e) {
+      print('An error occurred: $e');
+    }
+  }
+
+  Future<void> rebootDeviceForAndroid() async {
+    print("reboot$globleTopic");
+    try {
+      publish(globleTopic, "success");
+      await platform.invokeMethod('rebootDevice');
+    } on PlatformException catch (e) {
+      print("Failed to reboot device: ${e.message}");
+    }
+  }
+
+  Future<void> _rebootDeviceForMacOS() async {
+    try {
+      publish(globleTopic, "success");
+      final String result = await platformMacOS.invokeMethod('rebootDevice');
+      print(result);
+    } on PlatformException catch (e) {
+      print("Failed to reboot the device: '${e.message}'.");
+    }
+  }
+
+  void publish(String topic, String message) {
     var pubTopic = topic;
     final builder = MqttClientPayloadBuilder();
     builder.addString(message);
