a53a1f23d1494fc056b49eb626a10285e7e7274e
qasim212 <qasim.afzaal@techinoid.com>
2024-12-27 14:50:28 +0500

    init globle key

--- lib/utils/globle_variable.dart
+++ lib/utils/globle_variable.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 String globleTopic="";
 const platformMacOS = MethodChannel('com.example/deviceControl');
 const platform = MethodChannel('com.example/network');
+final GlobalKey boundaryKey = GlobalKey();
