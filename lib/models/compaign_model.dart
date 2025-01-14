bde41993c263e0f422dfa68b78a8f1a8f8ec4aed
qasim212 <qasim.afzaal@techinoid.com>
2025-01-14 18:20:08 +0500

    update cmapaign model

--- lib/models/compaign_model.dart
+++ lib/models/compaign_model.dart
@@ -8,244 +8,144 @@ String campaignModelToJson(CampaignModel data) => json.encode(data.toJson());
 class CampaignModel {
   String action;
   String sender;
-  String playbackType;
-  List<Data> data;
+  Data data;
 
   CampaignModel({
     required this.action,
     required this.sender,
-    required this.playbackType,
     required this.data,
   });
 
   factory CampaignModel.fromJson(Map<String, dynamic> json) => CampaignModel(
         action: json["action"],
         sender: json["sender"],
-        playbackType: json["playback_type"],
-        data: List<Data>.from(json["data"].map((x) => Data.fromJson(x))),
+        data: Data.fromJson(json["data"]),
       );
 
   Map<String, dynamic> toJson() => {
         "action": action,
         "sender": sender,
-        "playback_type": playbackType,
-        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+        "data": data.toJson(),
       };
 }
 
 class Data {
-  int campaignId;
-  Resolution resolution;
-  CampaignSettings campaignSettings;
-  CampaignSchedule campaignSchedule;
-  List<Zone> zones;
+  bool success;
+  String message;
+  List<PlayerCampaign> playerCampaigns;
 
   Data({
-    required this.campaignId,
-    required this.resolution,
-    required this.campaignSettings,
-    required this.campaignSchedule,
-    required this.zones,
+    required this.success,
+    required this.message,
+    required this.playerCampaigns,
   });
 
   factory Data.fromJson(Map<String, dynamic> json) => Data(
+        success: json["success"],
+        message: json["message"],
+        playerCampaigns: List<PlayerCampaign>.from(
+            json["playerCampaigns"].map((x) => PlayerCampaign.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
+        "playerCampaigns":
+            List<dynamic>.from(playerCampaigns.map((x) => x.toJson())),
+      };
+}
+
+class PlayerCampaign {
+  String playbackType;
+  String campaignId;
+  String campaignName;
+  Resolution resolution;
+  CampaignSchedule campaignSchedule;
+  CampaignSettings campaignSettings;
+  List<Zone> zones;
+
+  PlayerCampaign({
+    required this.playbackType,
+    required this.campaignId,
+    required this.campaignName,
+    required this.resolution,
+    required this.campaignSchedule,
+    required this.campaignSettings,
+    required this.zones,
+  });
+
+  factory PlayerCampaign.fromJson(Map<String, dynamic> json) => PlayerCampaign(
+        playbackType: json["playback_type"],
         campaignId: json["campaign_id"],
+        campaignName: json["campaign_name"],
         resolution: Resolution.fromJson(json["resolution"]),
-        campaignSettings: CampaignSettings.fromJson(json["campaign_settings"]),
         campaignSchedule: CampaignSchedule.fromJson(json["campaign_schedule"]),
+        campaignSettings: CampaignSettings.fromJson(json["campaign_settings"]),
         zones: List<Zone>.from(json["zones"].map((x) => Zone.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
+        "playback_type": playbackType,
         "campaign_id": campaignId,
         "resolution": resolution.toJson(),
-        "campaign_settings": campaignSettings.toJson(),
         "campaign_schedule": campaignSchedule.toJson(),
+        "campaign_settings": campaignSettings.toJson(),
         "zones": List<dynamic>.from(zones.map((x) => x.toJson())),
       };
 }
 
 class CampaignSchedule {
   bool alwaysPlay;
-  Period period;
+  dynamic period;
 
   CampaignSchedule({
     required this.alwaysPlay,
-    required this.period,
+    this.period,
   });
 
   factory CampaignSchedule.fromJson(Map<String, dynamic> json) =>
       CampaignSchedule(
         alwaysPlay: json["always_play"],
-        period: Period.fromJson(json["period"]),
+        period: json["period"],
       );
 
   Map<String, dynamic> toJson() => {
         "always_play": alwaysPlay,
-        "period": period.toJson(),
-      };
-}
-
-class Period {
-  Days days;
-  Time time;
-  Date date;
-
-  Period({
-    required this.days,
-    required this.time,
-    required this.date,
-  });
-
-  factory Period.fromJson(Map<String, dynamic> json) => Period(
-        days: Days.fromJson(json["days"]),
-        time: Time.fromJson(json["time"]),
-        date: Date.fromJson(json["date"]),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "days": days.toJson(),
-        "time": time.toJson(),
-        "date": date.toJson(),
-      };
-}
-
-class Date {
-  DateTime start;
-  DateTime end;
-
-  Date({
-    required this.start,
-    required this.end,
-  });
-
-  /// Custom method to parse date strings in MM/dd/yyyy format
-  static DateTime parseDate(String dateStr) {
-    final parts = dateStr.split('/');
-    if (parts.length == 3) {
-      return DateTime(
-        int.parse(parts[2]), // Year
-        int.parse(parts[0]), // Month
-        int.parse(parts[1]), // Day
-      );
-    }
-    throw FormatException("Invalid date format", dateStr);
-  }
-
-  factory Date.fromJson(Map<String, dynamic> json) => Date(
-        start: parseDate(json["start"]),
-        end: parseDate(json["end"]),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "start": "${start.month}/${start.day}/${start.year}",
-        "end": "${end.month}/${end.day}/${end.year}",
-      };
-}
-
-class Days {
-  bool sunday;
-  bool monday;
-  bool tuesday;
-  bool wednesday;
-  bool thursday;
-  bool friday;
-  bool saturday;
-
-  Days({
-    required this.sunday,
-    required this.monday,
-    required this.tuesday,
-    required this.wednesday,
-    required this.thursday,
-    required this.friday,
-    required this.saturday,
-  });
-
-  factory Days.fromJson(Map<String, dynamic> json) => Days(
-        sunday: json["sunday"],
-        monday: json["monday"],
-        tuesday: json["tuesday"],
-        wednesday: json["wednesday"],
-        thursday: json["thursday"],
-        friday: json["friday"],
-        saturday: json["saturday"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "sunday": sunday,
-        "monday": monday,
-        "tuesday": tuesday,
-        "wednesday": wednesday,
-        "thursday": thursday,
-        "friday": friday,
-        "saturday": saturday,
-      };
-}
-
-class Time {
-  String from;
-  String to;
-
-  Time({
-    required this.from,
-    required this.to,
-  });
-
-  factory Time.fromJson(Map<String, dynamic> json) => Time(
-        from: json["from"],
-        to: json["to"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "from": from,
-        "to": to,
-      };
-}
-
-class Resolution {
-  int width;
-  int height;
-
-  Resolution({
-    required this.width,
-    required this.height,
-  });
-
-  factory Resolution.fromJson(Map<String, dynamic> json) => Resolution(
-        width: json["width"],
-        height: json["height"],
-      );
-
-  Map<String, dynamic> toJson() => {
-        "width": width,
-        "height": height,
+        "period": period,
       };
 }
 
 class CampaignSettings {
-  int duration;
-  bool loop;
   String transition;
+  String duration;
+  bool loop;
 
   CampaignSettings({
+    required this.transition,
     required this.duration,
     required this.loop,
-    required this.transition,
   });
 
   factory CampaignSettings.fromJson(Map<String, dynamic> json) =>
       CampaignSettings(
+        transition: json["transition"],
         duration: json["duration"],
         loop: json["loop"],
-        transition: json["transition"],
       );
 
   Map<String, dynamic> toJson() => {
+        "transition": transition,
         "duration": duration,
         "loop": loop,
-        "transition": transition,
       };
+}
+
+class Resolution {
+  Resolution();
+
+  factory Resolution.fromJson(Map<String, dynamic> json) => Resolution();
+
+  Map<String, dynamic> toJson() => {};
 }
 
 class Zone {
@@ -318,25 +218,25 @@ class MediaItem {
 }
 
 class Settings {
-  int duration;
-  bool loop;
+  String duration;
   String transition;
+  bool loop;
 
   Settings({
     required this.duration,
-    required this.loop,
     required this.transition,
+    required this.loop,
   });
 
   factory Settings.fromJson(Map<String, dynamic> json) => Settings(
         duration: json["duration"],
-        loop: json["loop"],
         transition: json["transition"],
+        loop: json["loop"],
       );
 
   Map<String, dynamic> toJson() => {
         "duration": duration,
-        "loop": loop,
         "transition": transition,
+        "loop": loop,
       };
 }
