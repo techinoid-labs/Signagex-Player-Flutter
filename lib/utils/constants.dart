da4c95b44e08909a1b1c2beb91efea3f8cd20c37
fizaamjadtechinoid <fiza.a@techinoid.com>
2026-07-06 18:50:15 +0500

    Build-linux

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,6 +5,8 @@ const int mqttPort = 1883;
 const String baseurl = "https://stage.signagexai.com/v1/";
 const String adCampaignProofOfPlayPath = "player/ad-campaign-proof-of-play";
 const String port = "3002/";
+
+
 bool isIos = Platform.isIOS;
 bool isMac = Platform.isMacOS;
 Map<String, dynamic> deviceInfoMap = {
